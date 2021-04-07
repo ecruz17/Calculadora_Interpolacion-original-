5a28ab460b84a07597bd0ebbfd52ee94918b6bd9
Gustavo Higuera <ghiguera@ucol.mx>
2021-04-07 15:45:56 -0500

    implemented interpolacionLineal

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ window.addEventListener("load", function() {
     const LOADER = document.querySelector(".loader");
     LOADER.className += ' hidden';
 });
+
+
+export default class App{
+
+//Formulas de interpolación
+interpolacionLineal(x,fxreal,x0,fx0,x1,fx1){
+    let fxinterpolado= fx0 + ((fx1-fx0)/x1-x0)(x-x0);
+    if(fxreal!==0){
+        let errorVerdadero= fxinterpolado-fxreal;
+        let errorRelativoPorcentual= (errorVerdadero/fxreal)(100);
+        return errorRelativoPorcentual;
+    }
+    return fxinterpolado;
+
+}
+}
+
+let app= new App();
+
+console.log(interpolacionLineal(6,0,5,14,7,22));
