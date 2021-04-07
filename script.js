4a76e2b0ecf3e305cd65682627c3a48a2ecf4371
ecruz17 <ecruz17@ucol.mx>
2021-04-07 15:23:06 -0500

    cleared html and script file

--- script.js
+++ script.js
@@ -2,17 +2,3 @@ window.addEventListener("load", function() {
     const LOADER = document.querySelector(".loader");
     LOADER.className += ' hidden';
 });
-
-let fx0 = document.getElementById("fx0");
-let fx1 = document.getElementById("fx1");
-let x = document.getElementById("x");
-let x0 = document.getElementById("x0");
-let x1 = document.getElementById("x1");
-
-
-
-
-document.getElementById("calcular").addEventListener("calcu", function() {
-    let res = (fx0 + ((fx1-fx0)/(x1-x0)))*(x-x0);
-    console.log(res);
-});
