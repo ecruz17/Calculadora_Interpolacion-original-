c9db96c5466b7d38f0a5b623fb376e14c4617561
Gustavo Higuera <ghiguera@ucol.mx>
2021-04-12 17:23:25 -0500

    added some functions

--- script.js
+++ script.js
@@ -8,17 +8,39 @@ window.addEventListener("load", function() {
 export default class App{
 
 //Formulas de interpolación
-interpolacionLineal(x,fxreal,x0,fx0,x1,fx1){
+interpolacionLineal(x,x0,fx0,x1,fx1){
     let fxinterpolado= fx0 + ((fx1-fx0)/x1-x0)(x-x0);
+    return fxinterpolado;
+
+}
+
+obtenerError(fxreal,fxinterpolado){
     if(fxreal!==0){
         let errorVerdadero= fxinterpolado-fxreal;
         let errorRelativoPorcentual= (errorVerdadero/fxreal)(100);
         return errorRelativoPorcentual;
     }
-    return fxinterpolado;
+}
+
+interpolacionCuadratica(x,x0,fx0,x1,fx1,x2,fx2){
+    let b0=fx0;
+    let b1=(fx1-fx0)/(x1-x0);
+    let b2=(((fx2-fx1)/x2-x1)-((fx1-fx0)/x1-x0))/(x2-x0);
+    return b0+(b1)(x-x0)+(b2)(x-x0)(x-x1);
+}
+
+interpolacionLagrange1erOrden(x,x0,fx0,x1,fx1){
+    return ((x-x1)/(x0-x1))(fx0)+((x-x0)/(x1-x0))(fx1);
+}
+
+interpolacionLagrange2doOrden(x,x0,fx0,x1,fx1,x2,fx2){
+    
+}
+
+
 
 }
-}
+
 
 let app= new App();
 
